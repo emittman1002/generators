e23b36b6bdc29693c875d87e5ae6dacfaa59785a
Edward Mittman <emittman1002@hotmail.com>
2016-07-17 22:37:35 -0500

    Work on member generator set aside for breakfix

---
+++ member-generator/src/main/java/org/mittman/generator/member/MemberData.java
@@ -0,0 +1,89 @@
+package org.mittman.generator.member;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+
+public class MemberData {
+	@Setter(AccessLevel.PACKAGE)
+	private Random random;
+	
+	private String []firstNames = {
+			"James",
+			"Aaron",
+			"Melinda",
+			"Xio Mi",
+			"Hector",
+			"Geraldine"
+	};
+	
+	private String []middleNames = {
+			null,
+			"H",
+			"B",
+			"T",
+			"A",
+			"L"
+	};
+	
+	private String []lastNames = {
+			"Addison",
+			"Smith",
+			"Kling",
+			"Labowski",
+			"Li",
+			"Rodriguez"
+	};
+
+	public MemberData() {
+		random = new Random();
+	}
+
+	public String nextFirstName() {
+		return firstNames[random.nextInt(firstNames.length)];
+	}
+	
+	public String nextMiddleName() {
+		return middleNames[random.nextInt(middleNames.length)];
+	}
+	
+	public String nextLastName() {
+		return lastNames[random.nextInt(lastNames.length)];
+	}
+
+	public String nextSsn() {
+		StringBuilder sb = new StringBuilder(11);
+
+		while(sb.length()==0 || "000-00-0000".equals(sb.toString())) {
+			sb.setLength(0);
+			
+			for(int i=0; i<3; ++i) {
+				sb.append(random.nextInt(10));
+			}
+			sb.append("-");
+			for(int i=0; i<2; ++i) {
+				sb.append(random.nextInt(10));
+			}
+			sb.append("-");
+			for(int i=0; i<4; ++i) {
+				sb.append(random.nextInt(10));
+			}
+		}
+
+		return sb.toString();
+	}
+	
+	public LocalDate nextDateOfBirth() {
+		final int START = 1930;
+		
+		int offset = LocalDate.now().getYear() - START;
+		
+		int yr = random.nextInt(offset) + START;
+		int dayOfYear = random.nextInt(365);
+		
+		return LocalDate.ofYearDay(yr, dayOfYear);
+	}
+	
+}
